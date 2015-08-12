6ffa9cd076d08609978bbcfbb9a5c09568b36ed5
Mitchell Hines <mitchell.hines@irmplc.com>
2015-08-12 11:36:26 +0100

    Added new feature to search for unique GET requests containing specific search string

--- burppy.py
+++ burppy.py
@@ -73,14 +73,36 @@ def get_header_content(r, hdr, verbose):
 			print "\033[1;32m[Request  >]\033[0m %s" % req[hdrs.index(h)]
 			print "\033[1;32m[Response ?]\033[0m %s\n" % h
 
+
+def get_uniq_GET_by_search(r, search):
+
+	results = []
+
+	print "\033[1;32m[+]\033[0m Searching GET requests for unique instances of term '%s'" % search
+
+	for p in r:
+		t = p[0].split("\r")[0].split(" ")[1].split("?")[0]
+		if (search in t) and (t not in results):
+			results.append(t)
+
+	print "\033[1;32m[+]\033[0m Done"
+
+	for res in results:
+
+		print res	
+
 def usage():
 
-	print "Usage: %s\n\nOptions:\n\t-i\tInput file\n\t-H\tHeader to search (This is case sensitive! I.E 'Server' not 'server')\n\t-v\tVerbose (This is a lot of output)\n" % sys.argv[0]
+	print "Usage: %s\n\nOptions:\n\t-i\tInput file\n\t-H\tHeader to search (This is case sensitive! I.E 'Server' not 'server')\n\t-G\t[search term] (Search GET request URLS for string, return uniques)\n\t-v\tVerbose (This is a lot of output)\n" % sys.argv[0]
 
 if __name__ == "__main__":
 
+	header_search = False
+	GET_search = False
+
 	if len(sys.argv) > 4:
 		verbose = False
+		
 		if "-i" in sys.argv:
 			in_file = sys.argv[sys.argv.index("-i")+1]
 			xml = init_file(in_file)
@@ -88,11 +110,21 @@ if __name__ == "__main__":
 		
 		if "-H" in sys.argv:
 			header = sys.argv[sys.argv.index("-H")+1]
+			header_search = True
 
+		if "-G" in sys.argv:
+			getsearch = sys.argv[sys.argv.index("-G")+1]
+			GET_search = True
+		
 		if "-v" in sys.argv:
 			verbose = True
 
-		get_header_content(req_resp, header, verbose)
+		if (header_search):
+			get_header_content(req_resp, header, verbose)
+	
+		if (GET_search):
+			get_uniq_GET_by_search(req_resp, getsearch)
+
 	else:
 		usage()
 		exit(1)
